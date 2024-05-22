b85b245e96e4c7e4fb10666bbb51cdea836cc724
Seeba184 <seenasimkani@gmail.com>
2024-05-22 09:50:27 -0400

    Add method for choosing random word from text file

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
+
 class Hangman
   def initialize; end
+end
+
+def choose_random_word(text_file)
+  File.open text_file do |file|
+    file.readlines.sample.chomp
+  end
 end
