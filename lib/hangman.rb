d7c1c707ca0ea1b514dfd34409319a3562a3c27f
Seeba184 <seenasimkani@gmail.com>
2024-05-22 12:22:21 -0400

    Add method to display correct words guessed, guesses remaining, and guesses tried

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Hangman
-  attr_accessor :wrong_guesses_remaining, :total_guesses, :wrong_guesses, :correct_guesses
+  attr_accessor :wrong_guesses_remaining, :total_guesses, :wrong_guesses, :correct_guesses, :random_word
 
   def initialize
     @wrong_guesses_remaining = 6
@@ -17,8 +17,18 @@ class Hangman
     end
   end
 
+  def display
+    @random_word = choose_random_word('words.txt').split('')
+    correct_guesses = @random_word.map { |_| '_ ' }
+    print '------------------------------------------------------------'
+    print "------------------------------------------------------------\n\n\n\n\n\n\n\n\n\n\n"
+    print "#{correct_guesses.join}\t"
+    print "Wrong Guesses Remaining: #{wrong_guesses_remaining}\t Total Guesses: #{total_guesses}\t"
+    print "Tried: #{wrong_guesses.join(', ')}\n"
+  end
+
   def player_guess
-    print 'Guess a letter: '
+    print "\n\nGuess a letter: "
     player_input = gets.chomp
     unless [*'a'..'z'].include?(player_input.downcase)
       puts 'Please enter a valid letter.'
