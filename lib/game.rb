bff5ef889c5f226edf2d416b0fc38c22a79250b5
Seeba184 <seenasimkani@gmail.com>
2024-05-23 12:10:14 -0400

    Add loop for playing multiple games

--- lib/game.rb
+++ lib/game.rb
@@ -2,6 +2,13 @@ require_relative 'hangman'
 
 class Game
   def play_game
-    Hangman.new.play
+    loop do
+      Hangman.new.play
+      puts 'Enter 1 to play again or 0 to exit'
+      input = gets.chomp
+      break unless input == '1'
+    end
   end
 end
+
+Game.new.play_game
