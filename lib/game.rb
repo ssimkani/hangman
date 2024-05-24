0c5fc9cd95a7c00e2545e31eb6721ebcdb2628ae
Seeba184 <seenasimkani@gmail.com>
2024-05-24 07:21:04 -0400

    Add no saved games message if directory is empty

--- lib/game.rb
+++ lib/game.rb
@@ -20,10 +20,16 @@ class Game
       puts "To save your game, enter 2 when prompted to guess a letter.\n\n"
       play_game
     when '2'
-
+      files = []
       puts "Enter the number of the saved game you would like to load:\n\n"
       Dir.open('saved_games') do |dir|
-        dir.each_child { |file| puts file }
+        dir.each_child { |file| files << file }
+        if files.empty?
+          puts 'No saved games found.'
+          exit
+        else
+          files.each { |f| puts f }
+        end
       end
       load_game(gets.chomp)
 
@@ -33,18 +39,13 @@ class Game
   end
 
   def play_game
-    loop do
-      @hangman.play
-      print 'Enter 1 to play again or 0 to exit:  '
-      input = gets.chomp
-      break unless input == '1'
-    end
+    @hangman.play
   end
 
   def load_game(num)
     File.open("saved_games/save_#{num}.yml", 'r') do |file|
       yaml_string = file.read
-      object = YAML.safe_load(yaml_string)
+      object = YAML.safe_load(yaml_string, permitted_classes: [Hangman])
       object.play
     end
   end
