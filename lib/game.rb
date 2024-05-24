0ff5ddd1d2b1c6027797c452c73f75a77cc84ce6
Seeba184 <seenasimkani@gmail.com>
2024-05-24 06:45:00 -0400

    Fix outputting . and .. when asked what save to load

--- lib/game.rb
+++ lib/game.rb
@@ -10,29 +10,25 @@ class Game
 
   def welcome_message
     puts 'WELCOME TO HANGMAN'
-    begin
-      print 'Enter 1 to play, 2 to load a game, or 0 to exit:  '
-      input = gets.chomp
-      case input
-      when '0'
-        exit
-      when '1'
-        puts "To save your game, enter 2 when prompted to guess a letter.\n\n"
-        play_game
-      when '2'
-        begin
-          print 'Enter the number of the saved game you would like to load:  '
-          load_game(gets.chomp)
-        rescue StandardError
-          puts 'No saved game with that number. Please try again.'
-          retry
-        end
-      else
-        raise StandardError
+
+    print 'Enter 1 to play, 2 to load a game, or 0 to exit:  '
+    input = gets.chomp
+    case input
+    when '0'
+      exit
+    when '1'
+      puts "To save your game, enter 2 when prompted to guess a letter.\n\n"
+      play_game
+    when '2'
+
+      puts "Enter the number of the saved game you would like to load:\n\n"
+      Dir.open('saved_games') do |dir|
+        dir.each_child { |file| puts file }
       end
-    rescue StandardError
-      puts 'Please enter 1, 2, or 0.'
-      retry
+      load_game(gets.chomp)
+
+    else
+      exit
     end
   end
 
