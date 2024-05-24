33fa474abf6016a2ee9c052ea904f405bd9470c5
Seeba184 <seenasimkani@gmail.com>
2024-05-24 12:48:35 -0400

    Add class and method comments

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# This class contains the main logic for playing hangman. It includes methods such as
+# displaying the game and handling the player guesses.
 class Hangman
   attr_accessor :guesses_remaining, :total_guesses, :guesses_tried, :display_correct_guesses, :player_input
 
@@ -14,6 +16,7 @@ class Hangman
   end
 
   def play
+    # Main method for playing the game and displaying if the user won or lost.
     display until guesses_remaining.zero? || (word == display_correct_guesses)
     display
     puts "\n\nGAME OVER"
@@ -22,6 +25,7 @@ class Hangman
   end
 
   def display_hangman
+    # Displays the hangman stage based on the number of guesses remaining.
     stages = [
       '
      --------
@@ -91,6 +95,7 @@ class Hangman
   end
 
   def display
+    # Displays the hangman, the word to be guessed, total guesses, and guesses tried
     print '------------------------------------------------------------'
     print "------------------------------------------------------------\n\n"
     puts display_hangman
@@ -109,12 +114,14 @@ class Hangman
   protected
 
   def choose_word(text_file)
+    # Chooses a random word from the word.txt file.
     File.open text_file do |file|
       file.readlines.sample.chomp.upcase
     end
   end
 
   def player_guess
+    # Gets the players input and checks if it is valid or not
     print "\n\nGuess a letter: "
     @player_input = gets.chomp.upcase
     if ([*'A'..'Z'].include?(player_input) && !guesses_tried.include?(player_input)) || player_input == '2'
@@ -126,6 +133,7 @@ class Hangman
   end
 
   def check_guess(letter)
+    # Determines if the letter guessed is in the word or the player wants to save their game
     if word.include?(letter)
       replace(letter)
     elsif letter == '2'
@@ -142,6 +150,7 @@ class Hangman
   end
 
   def replace(letter)
+    # Replaces the '_' in the display word with the guessed letter
     arr = []
     word.each_with_index { |char, index| arr << index if char == letter }
     arr.each { |index| display_correct_guesses[index] = letter }
@@ -151,6 +160,7 @@ class Hangman
   end
 
   def save_game(object, num)
+    # Saves the game to a yaml file in the saved_games directory
     yaml_string = object.to_yaml
     File.open("saved_games/save_#{num}.yml", 'w') do |file|
       file.write(yaml_string)
