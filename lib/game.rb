b52c894f44fc7237d06f06e10d557aefd7f64231
Seeba184 <seenasimkani@gmail.com>
2024-05-24 12:39:44 -0400

    Add class and method comments

--- lib/game.rb
+++ lib/game.rb
@@ -3,12 +3,17 @@
 require 'yaml'
 require_relative 'hangman'
 
+# the Game class initializes a new hangman class and contains methods for
+# loading the saved games, looping multiple hangman games, and displaying a welcome screen.
 class Game
   def initialize
+    # a new instance of the hangman class
     @hangman = Hangman.new
   end
 
   def welcome_message
+    # Displays a welcome message and prompts the user to choose to play a new game,
+    # load a saved game, or exit the program.
     puts 'WELCOME TO HANGMAN'
 
     print 'Enter 1 to play, 2 to load a game, or 0 to exit:  '
@@ -39,6 +44,9 @@ class Game
   end
 
   def play_game
+    # This method starts a new game of hangman by calling the play method on
+    # the hangman class. After a game is finishes, it then prompts the user if they
+    # would like to play another game.
     loop do
       @hangman.play
       puts "\n\nEnter 1 to play again or 0 to exit:"
@@ -51,6 +59,7 @@ class Game
   end
 
   def load_game(num)
+    # Loads the chosen yaml file from the saved_games directory based on the save number.
     File.open("saved_games/save_#{num}.yml", 'r') do |file|
       yaml_string = file.read
       @hangman = YAML.safe_load(yaml_string, permitted_classes: [Hangman])
